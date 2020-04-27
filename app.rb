60355687e6769573a2f81e18905ebdaf0dcf60c7
careful-illustrator-5297 <anthonyroma741@gmail.com>
2020-04-27 16:32:49 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,7 +11,18 @@ class App < Sinatra::Base
   (params[:number].to_i ** 2).to_s
   end
   
-  get '/say/:number/:phrase'
+  
+  get '/say/:number/:phrase' do
+    answer = ''
+
+  params[:number].to_i.times do
+    answer += params[:phrase]
+  end
+
+    answer
+  end
+
+  get
 
 
 end
