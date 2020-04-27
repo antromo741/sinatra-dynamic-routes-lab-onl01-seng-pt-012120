e8c82c3fbb5e230c1a1b450ad43e03e1e0d12050
careful-illustrator-5297 <anthonyroma741@gmail.com>
2020-04-27 16:21:22 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,12 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
-
+  
+  get '/reversename/:name' do
+    params[:name].reverse
+  end
+  
+  
+  
+  
 end
